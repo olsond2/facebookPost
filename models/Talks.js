c414b18487d4e616a98a1c28ae8d8db3c6410fa8
Tyler <tdoermann@gmail.com>
2017-04-17 23:29:58 -0600

    fixed model type

--- models/Talks.js
+++ models/Talks.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 var CommentSchema = new mongoose.Schema({
-  title: String,
+  email: String,
+  name: String,
+  time: Number,
+  photoURL: String,
   upvotes: {type: Number, default: 0},
 });
 CommentSchema.methods.upvote = function(cb) {
