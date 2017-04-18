9f7da9add29d4ac2ca349d31813b9628762f3eea
Dallin Olson <dallin.olson@gmail.com>
2017-04-18 18:58:58 +0000

    more CSS

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -69,18 +69,58 @@ app.controller('MainCtrl', [
     	$scope.speakerContent = '';
     	$scope.urlContent = '';
     };
-  */    
+  */  
+   
+      
+    
+ /*   timeSinceFunction = function() {
+        var elapsedTime;
+        elapsedTime = $scope.currentTime -$scope.firebaseUser.time;
+        var m = Math.floor((elapsedTime / 1000) / 60);
+        var h = Math.floor(m / 60);
+        if (m > 60) {
+            $scope.timeSince = h + "hrs ago";
+        }
+        else if (m < 60) {
+            console.log("welp");
+            $scope.timeSince = m + "min ago";
+        }
+    }*/
+    
+      
+    $(document).ready(function() {
+        var d = new Date();
+        var n = d.getTime();
+        $scope.currentTime = n;
+        var elapsedTime;
+    elapsedTime = $scope.currentTime - $scope.comments.time;
+           console.log(elapsedTime);
+        $scope.comments.forEach(function(e) {
+            console.log(e.time);
+        })
+      
+    if ((elapsedTime / 1000)/60<60) {
+        console.log("OUTPUT in Minutes");
+    }
+    });
+      
+      
+      
+    $scope.Math = window.Math;
+      
     $scope.addComment = function() {
         if($scope.formContent == '') {
             return;
         }
+        var d = new Date();
+        var n = d.getTime();
         $scope.create({
             postedComment: $scope.formContent,
             upvotes: 0,
             email: $scope.firebaseUser.email,
             name: $scope.firebaseUser.displayName,
             photoURL: $scope.firebaseUser.photoURL,
-            time: 0,
+            time: n,
         });
         console.log("hello"+$scope.formContent);
         $scope.formContent = '';
@@ -88,7 +128,8 @@ app.controller('MainCtrl', [
     
     $scope.create = function(comment) {
     return $http.post('/comments', comment).success(function(data){
-      $scope.comments.push(data);
+        //$scope.comments.push(data);
+      $scope.comments.splice(0,0,data);
       });
     };
 
@@ -116,6 +157,8 @@ app.controller('MainCtrl', [
 	$scope.getAll();
     };
 
+      
+       
     $scope.getAll();
 
   }
